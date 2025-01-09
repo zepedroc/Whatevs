89395d70c08e25ae5d8596252d9d5a747be9adae
zepedroc <jose.mota@emergn.com>
2025-01-09 08:42:06 +0000

    Fix locale var error

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,6 +4,7 @@ export default getRequestConfig(async (props) => {
   const locale = await props.requestLocale;
 
   return {
+    locale,
     messages: (await import(`../messages/${locale}/messages.json`)).default,
   };
 });
