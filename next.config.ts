670fc1d1893f8c2e27646eb034e7326317c43aa2
José Mota <jose.mota@emergn.com>
2025-10-21 17:37:19 +0100

    React compiler

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-import createNextIntlPlugin from 'next-intl/plugin';
 import type { NextConfig } from 'next/dist/server/config';
+
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    reactCompiler: true,
+  },
   env: {
     HUGGINGFACE_INFERENCE_TOKEN: process.env.HUGGINGFACE_INFERENCE_TOKEN,
   },
